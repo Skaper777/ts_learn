d19f54d36d2f69247755ba3ccfa901a923151a44
Sergey <skaper777@gmail.com>
2022-07-05 17:05:36 +0300

    feat: annotations 2 (funcs, arrays, tupples, objects)

---
+++ annotations/arrays.ts
@@ -0,0 +1,22 @@
+const myColors = ['red', 'green', 'yellow']
+
+const fruitsByColor = [
+    ['tomato'],
+    ['apple'],
+    ['lemon']
+]
+
+// Help with inference when extracting values 
+const color = myColors[0]
+const color1 = myColors.pop()
+
+// Prevent incompatible values 
+myColors.push(null)
+
+// Help with 'map'
+myColors.map((item: string): string => {
+    return item
+})
+
+// Flexible types 
+const importantDates = [new Date(), '2021-11-11']

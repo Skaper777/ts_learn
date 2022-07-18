429441e0979788406f95af571d65bd428f1c39e0
Sergey <skaper777@gmail.com>
2022-07-18 17:04:57 +0300

    finish map project

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker';
+import { Mappable } from './Map';
 
-export class Company {
+export class Company implements Mappable {
   name: string
   catchPhrase: string
   location: {
@@ -15,5 +16,12 @@ export class Company {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude())
     }
+  }
+
+  markerContent(): string {
+    return `
+      <h2>Company name: ${this.name}</h2>
+      <h3>Company phrase: ${this.catchPhrase}</h3>
+    `
   }
 }
--- maps/src/Map.ts
+++ maps/src/Map.ts
@@ -1,5 +1,13 @@
 import {} from 'googlemaps'
 
+export interface Mappable {
+  location: {
+    lat: number
+    lng: number
+  }
+  markerContent(): string
+}
+
 export class Map {
   private googleMap: google.maps.Map
 
@@ -10,6 +18,24 @@ export class Map {
         lat: 0,
         lng: 0
       }
+    })
+  }
+
+  addMarker(point: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: point.location.lat,
+        lng: point.location.lng
+      }
+    })
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: point.markerContent()
+      })
+
+      infoWindow.open(this.googleMap, marker)
     })
   }
 }

c9d54581aecf537f4338f653c3b8a5b6855c501e
Sergey <skaper777@gmail.com>
2022-09-07 12:26:02 +0300

    abstract classes and full sort project

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -4,11 +4,9 @@ import { CharactersCollection } from "./CharactersCollection"
 
 const numbersCollection = new NumbersCollection([2, -10, 99, 1])
 const charatersCollection = new CharactersCollection('asXsdw')
-const sorter1 = new Sorter(numbersCollection)
-const sorter2 = new Sorter(charatersCollection)
 
-sorter1.sort()
-sorter2.sort()
+numbersCollection.sort()
+charatersCollection.sort()
 
 console.log(numbersCollection.data)
 console.log(charatersCollection.data)
